496d763138896662f3b89fb99722f688313645eb
destinerikanb <destinerikanb@gmail.com>
2023-06-02 03:21:24 +0700

    Implement while loop in book reading's program

---
+++ program/while_loop.py
@@ -0,0 +1,15 @@
+# Looping
+# While loop
+# Re-write book reading's program using while loop
+
+print("=====================================================")
+print("                  READING BOOK PROGRAM              ")
+books = 10
+read_book = 0
+
+while read_book < books:
+    print(f"Book {read_book+1} has been read")
+    read_book += 1
+
+print("Finish reading all books")
+print("=====================================================")
