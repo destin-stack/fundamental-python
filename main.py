769b7d091affc509e52895dd34748bd9fa261d31
destinerikanb <destinerikanb@gmail.com>
2023-06-02 03:07:58 +0700

    Implement for loop in book reading's program

--- main.py
+++ main.py
@@ -31,3 +31,19 @@ if budi_money > 0:
 else:
     print("There's no enough money")
 
+
+# Looping
+# For looping
+
+print("=====================================================")
+print("                  READING BOOK PROGRAM              ")
+books = 10
+read_book = 0
+
+print("Let's read books and get bunch of knowledge!!!")
+for i in range(0, books):
+    print(f"Book {i+1} has been read")
+    read_book += 1
+
+print("Finish reading all books")
+print("=====================================================")
